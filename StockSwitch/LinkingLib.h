62a3e686fb0c548cbb80955960767d32829e1a5a
MiNaka04 <nakashima.mizuho@wacom.co.jp>
2016-03-16 11:23:22 +0900

    ProjectUpload
    Uploadします

---
+++ StockSwitch/LinkingLib.h
@@ -0,0 +1,16 @@
+//
+//  LinkingLib.h
+//  StockSwitch
+//
+//  Created by mizuho on 2016/03/14.
+//  Copyright © 2016年 mizuho. All rights reserved.
+//
+
+#ifndef LinkingLib_h
+#define LinkingLib_h
+
+
+#import <LinkingLibrary/LinkingLibrary.h>
+
+
+#endif /* LinkingLib_h */
