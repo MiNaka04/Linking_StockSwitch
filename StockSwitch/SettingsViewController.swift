1cb62696c363af34b4eaadeb28f009963b6f9c26
iijijii <lilysatoo@gmail.com>
2016-03-22 22:55:52 +0900

    Add setButtonClicked

--- StockSwitch/SettingsViewController.swift
+++ StockSwitch/SettingsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var textBoxAlertDate: UITextField!
+    @IBOutlet weak var textBoxExpiryDate: UITextField!
+    @IBOutlet weak var textBoxFallPercentage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +24,8 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func setButtonClicked(sender: UIButton) {
+    
+    }
     
 }
