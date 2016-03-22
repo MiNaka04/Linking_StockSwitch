5e837c5ec0c0d0f5e218493f9d28c139e7746742
iijijii <lilysatoo@gmail.com>
2016-03-23 07:57:53 +0900

    Add SettingsData class

---
+++ StockSwitch/SettingsData.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsData.swift
+//  StockSwitch
+//
+//  Created by riri on 2016/03/23.
+//  Copyright © 2016年 mizuho. All rights reserved.
+//
+
+import Foundation
+
+public class SettingsData{
+    static var alertDate: Int = 3
+    static var expiryDate: Int = 7
+    static var fallPercentage: Int = 50
+}
